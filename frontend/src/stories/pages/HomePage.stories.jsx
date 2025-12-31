dc3fd8cd6dc74530f42eb317f0bbf32030c4352a
Phill Conrad <pconrad@cs.ucsb.edu>
2025-12-30 17:14:46 -0800

    pc - remove msw from HomePage stories

--- frontend/src/stories/pages/HomePage.stories.jsx
+++ frontend/src/stories/pages/HomePage.stories.jsx
@@ -1,7 +1,3 @@
-import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
-import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import { http, HttpResponse } from "msw";
-
 import HomePage from "main/pages/HomePage";
 
 export default {
@@ -11,44 +7,6 @@ export default {
 
 const Template = () => <HomePage />;
 
-export const LoggedOut = Template.bind({});
-LoggedOut.parameters = {
-  msw: {
-    handlers: [
-      http.get("/api/currentUser", () => {
-        return HttpResponse.json(null, { status: 403 });
-      }),
-      http.get("/api/systemInfo", () => {
-        return HttpResponse.json(systemInfoFixtures.showingNeither);
-      }),
-    ],
-  },
-};
+export const Default = Template.bind({});
+Default.parameters = {};
 
-export const LoggedInRegularUser = Template.bind({});
-LoggedInRegularUser.parameters = {
-  msw: {
-    handlers: [
-      http.get("/api/currentUser", () => {
-        return HttpResponse.json(apiCurrentUserFixtures.userOnly);
-      }),
-      http.get("/api/systemInfo", () => {
-        return HttpResponse.json(systemInfoFixtures.showingNeither);
-      }),
-    ],
-  },
-};
-
-export const LoggedInAdminUserShowingSwaggerAndH2Console = Template.bind({});
-LoggedInAdminUserShowingSwaggerAndH2Console.parameters = {
-  msw: {
-    handlers: [
-      http.get("/api/currentUser", () => {
-        return HttpResponse.json(apiCurrentUserFixtures.adminUser);
-      }),
-      http.get("/api/systemInfo", () => {
-        return HttpResponse.json(systemInfoFixtures.showingBoth);
-      }),
-    ],
-  },
-};
