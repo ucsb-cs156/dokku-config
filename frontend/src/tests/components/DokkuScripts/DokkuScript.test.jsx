097e178539acbfc5cad41d3465d349e8edaafa4b
Phill Conrad <pconrad@cs.ucsb.edu>
2026-01-02 15:18:23 -0800

    pc - initial hardcoded dokku for happycows

---
+++ frontend/src/tests/components/DokkuScripts/DokkuScript.test.jsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import DokkuScript from "main/components/DokkuScripts/DokkuScript";
+import { expect } from "vitest";
+
+describe("DokkuScript tests", () => {
+  test("has expected attributes", async () => {
+    render(<DokkuScript />);
+    const dokkuscript = screen.getByTestId("dokkuscript");
+    expect(dokkuscript).toHaveAttribute("data-testid", "dokkuscript");
+    expect(dokkuscript).toHaveAttribute("style", "white-space: pre;");
+  });
+
+  test("defaults", async () => {
+    // prettier-ignore
+    const expected = `
+      dokku apps:create happycows
+      dokku git:set happycows keep-git-dir true
+      dokku config:set --no-restart happycows PRODUCTION=true
+      dokku config:set --no-restart happycows SOURCE_REPO=https://github.com/ucsb-cs156/proj-happycows
+      dokku postgres:create happycows-db
+      dokku postgres:link happycows-db happycows
+      dokku git:sync happycows https://github.com/ucsb-cs156/proj-happycows main
+      dokku ps:rebuild happycows
+      dokku letsencrypt:set happycows email phtcon@ucsb.edu
+      dokku letsencrypt:enable happycows
+      dokku config:set happycows --no-restart GOOGLE_CLIENT_ID=get-value-from-google
+      dokku config:set happycows --no-restart GOOGLE_CLIENT_SECRET=get-value-from-google`;
+    render(<DokkuScript />);
+    const dokkuscript = screen.getByTestId("dokkuscript");
+    expect(dokkuscript).toHaveTextContent(expected, {
+      normalizeWhitespace: false,
+    });
+  });
+});
