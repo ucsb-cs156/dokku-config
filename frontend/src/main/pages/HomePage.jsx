cb12375f227a31aa99c68593e28cdce10de76d54
Phill Conrad <pconrad@cs.ucsb.edu>
2026-01-02 18:34:55 -0800

    pc - add form on home page and link to script output

--- frontend/src/main/pages/HomePage.jsx
+++ frontend/src/main/pages/HomePage.jsx
@@ -1,7 +1,16 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import DokkuScript from "main/components/DokkuScripts/DokkuScript";
+import DokkuScriptForm from "main/components/DokkuScripts/DokkuScriptForm";
+
+import { useState } from "react";
 
 export default function HomePage() {
+  const [params, setParams] = useState();
+
+  const callback = (params) => {
+    setParams(params);
+  };
+
   return (
     <BasicLayout>
       <div className="pt-2">
@@ -11,9 +20,13 @@ export default function HomePage() {
           at UC Santa Barbara.
         </p>
       </div>
+      <div>
+        <h2>Specify Dokku App</h2>
+        <DokkuScriptForm callback={callback} />
+      </div>
       <div className="pt-2">
         <h2>Generated Dokku Script</h2>
-        <DokkuScript />
+        <DokkuScript {...params} />
       </div>
     </BasicLayout>
   );
